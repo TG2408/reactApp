4fbc6916b72b51085e7c63c242ab3b66b382b85c
TG2408 <tanmaygupta2408@gmail.com>
2021-09-27 12:05:01 +0530

    modules 1 & 2

---
+++ src/module1.mjs
@@ -0,0 +1,6 @@
+import uin , {d,c,b} from './module2.mjs'
+
+console.log(uin);
+console.log(d);
+console.log(c);
+console.log(b);
---
+++ src/module2.mjs
@@ -0,0 +1,9 @@
+const a = "Tanmay";
+const b = "gupta";
+const c = "JAI MATA DI";
+const d = "RADHE RADHE";
+
+export default a;
+export {b};
+export {c};
+export {d}
